1c058d7da50aefba8899b50452179eae9949235f
julysNICK <julystube@gmail.com>
2023-09-07 16:31:47 -0300

    add shippingAdrress

---
+++ lib/screens/shipping_address.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShippingAddress extends StatelessWidget {
+  const ShippingAddress({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
